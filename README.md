de70d5c7f2c68c6ba1e2b7fc8cd28a28476a900c
paulgdm <paul.grandamme@gmail.com>
2020-12-05 13:47:19 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# InvAES
- C implementation of inverse AES 128bits
+# AES
+
+Decryption with AES 128bits in C
+
+## Compilation
+
+In a shell bash :
+
+```bash
+make
+```
+
+## Usage
+
+```bash
+sh decrypt.sh ciphertext.txt cleartext.txt
+```
--- uncrypt.sh
+++ uncrypt.sh
@@ -4,9 +4,7 @@ make all
 rm result.txt
 rm cleartext.txt
 
-#hexdump -e '16/1 "%02x " "\n"' cleartext.txt >> result.txt
-
-cat ./../ciphertext.txt | while  read ligne ; do
+cat $1.txt | while  read ligne ; do
 	echo $ligne
-  	./InvAES $ligne | xxd -r -p >> result.txt
+  	./InvAES $ligne | xxd -r -p >> $2.txt
 done
